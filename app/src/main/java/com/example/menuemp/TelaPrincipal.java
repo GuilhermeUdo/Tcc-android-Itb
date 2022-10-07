f3e599a39366973381282e86fc103c3822739cc2
GuilhermeUdo <guilhermeudo6@gmail.com>
2022-10-07 15:43:58 -0300

    ligação de tela terminação

--- app/src/main/java/com/example/menuemp/TelaPrincipal.java
+++ app/src/main/java/com/example/menuemp/TelaPrincipal.java
@@ -2,7 +2,9 @@ package com.example.menuemp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TelaPrincipal extends AppCompatActivity {
 
@@ -12,5 +14,10 @@ public class TelaPrincipal extends AppCompatActivity {
         setContentView(R.layout.activity_tela_principal);
 
         getSupportActionBar().hide();
+    }
+
+    public void deslogar1(View view) {
+        Intent intent = new Intent(this, FormLogin.class);
+        startActivity(intent);
     }
 }
